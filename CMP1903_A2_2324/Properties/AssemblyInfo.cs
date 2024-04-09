60712d62ce780255dea1d8a0ca56d15f0ac841ec
Lewis Page <lewisjet11@outlook.com>
2024-04-09 16:26:37 +0100

    Rename Project

--- CMP1903_A2_2324/Properties/AssemblyInfo.cs
+++ CMP1903_A2_2324/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CMP1903_A1_2324")]
+[assembly: AssemblyTitle("CMP1903_A2_2324")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CMP1903_A1_2324")]
+[assembly: AssemblyProduct("CMP1903_A2_2324")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
